8439d4e777a9ea1d47c94d8f352870133310afe9
Lee <nlkh@koreatech.ac.kr>
2021-02-19 10:57:31 +0900

    for jenkins test

--- basicAPI.py
+++ basicAPI.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 def hello() :
     return jsonify({
     "data": {        "id": 9604521,
-        "typeCode": 1234,
         "loginId": "dlrudgud08**@gmail.com",
         "ownerCode": "J534327",
         "createdAt": "2021-01-11 16:24:36"},
